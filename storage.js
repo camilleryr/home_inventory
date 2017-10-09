618399df2fadd0a83deeff32f0beb1e353f36e3d
Christopher Miller <camiller.yr@gmail.com>
2017-10-08 20:53:18 -0500

    Filled in object data

---
+++ storage.js
@@ -0,0 +1,69 @@
+const _76musicman = {
+    "name": "1976 Musicman",
+    "type": "instrument",
+    "location": "Storage Nook",
+    "description": "Pre Ernie Ball musicman bass - Used on most of my recordings"
+}
+
+const jackCassidy = {
+    "name": "Jack Cassidy Bass",
+    "type": "instrument",
+    "location": "Storage Nook",
+    "description": "Workhorse live bass - bought with money I won in a raffle at a Thortons"
+}
+
+const momsClassica = {
+    "name": "Moms Classical Guitar",
+    "type": "instrument",
+    "location": "Guest Bedroom",
+    "description": "Moms classical guitar"
+}
+
+const tvConsole = {
+    "name": "Mid Century TV Console",
+    "type": "furniture",
+    "location": "Living Room",
+    "description": "Mid centruy TV console I built after moving into our house"
+}
+
+const flossCabinet = {
+    "name": "Sarahs Floss Cabinet",
+    "type": "furniture",
+    "location": "Living Room",
+    "description": "Floss cabinet I built for Sarah for Christmas 2015"
+}
+
+const hexTable = {
+    "name": "Rotating Hex Table",
+    "type": "furniture",
+    "location": "Kitchen",
+    "description": "Rotating Hexagonal Table built after a piece seen in San Fransisco"
+}
+
+const vanity = {
+    "name": "Bathroom Vanity",
+    "type": "furniture",
+    "location": "Master Bath",
+    "description": "L Shaped bathroom vanity made from baltic birch pine built for master bath"
+}
+
+const weddingPhoto = {
+    "name": "Group Wedding Photo",
+    "type": "memory",
+    "location": "Kitchen",
+    "description": "Group photo from our wedding"
+}
+
+const marwencolPrint = {
+    "name": "Merwencol Print",
+    "type": "memory",
+    "location": "Guest Bedroom",
+    "description": "Print made from the movie Sarah and I saw on our first date"
+}
+
+const catBirdSeat = {
+    "name": "Cat Bird Seat Menu",
+    "type": "memory",
+    "location": "Kitchen",
+    "description": "Menu from my birthday trip to Cat Bird Seat"
+}
